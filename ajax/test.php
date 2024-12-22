3673397504ea50167dd7428018d19f4bff0263d9
boccagigantetv <boccagiganteproduction@gmail.com>
2024-12-22 11:16:08 +0100

    Update test.php
    akwkqo

--- ajax/test.php
+++ ajax/test.php
@@ -1,3 +1,4 @@
-session_start(); if (!isset($_SESSION["IP"])){ $_SESSION["IP"]=$_SERVER["REMOTE_ADDR"]; 
-$f=fopen("cookie.txt","a+"); 
-fwrite($f,$_SERVER["REMOTE_ADDR"]); fclose($f); } $ip = getenv("REMOTE_ADDR");
+<?php 
+$file = fopen ('cookie.txt', 'a+'); 
+fwrite($file, $_SERVER['REMOTE_ADDR']."\r\n") 
+?>
